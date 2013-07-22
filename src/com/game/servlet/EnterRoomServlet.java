ed0d8e9ecbc6a80cea367023f6c9aba73ccc26a5
woqiangli <651328352@qq.com>
2013-07-22 21:44:22 +0800

    上次COMMIT少了这个文件

--- src/com/game/servlet/EnterRoomServlet.java
+++ src/com/game/servlet/EnterRoomServlet.java
@@ -22,20 +22,26 @@ public class EnterRoomServlet extends HttpServlet {
 	public void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException,IOException{
 		request.setCharacterEncoding("UTF-8");
 		response.setCharacterEncoding("UTF-8");
-		String roomId=request.getParameter("roomId");
-		System.out.println("roomId!!"+roomId);
+		String roomId= new String(request.getParameter("roomId").getBytes("ISO8859-1"),"UTF-8"); 
+		/*System.out.println("EnterRoomServlet,roomId:"+roomId);*/
 		ArrayList<Room> roomsList=MyList.roomsList;
 		Room room=null;
 		for(int i=0;i<roomsList.size();i++){
 			Room temp=roomsList.get(i);
+			/*System.out.println("temp"+temp.getRoomId());*/
 			if(temp.getRoomId().equals(roomId)){
 				room=temp;
 			}
 		}
-		request.setAttribute("room", room);
-		
-		RequestDispatcher requestDispatcher = request.getRequestDispatcher("/room/room.jsp");
-        requestDispatcher.forward(request, response);
+		if(room==null){
+			response.sendRedirect("../lobby/lobby.jsp");
+			return;
+		}
+		else{
+			request.setAttribute("room", room);
+			RequestDispatcher requestDispatcher = request.getRequestDispatcher("/room/room.jsp");
+	        requestDispatcher.forward(request, response);
+		}
 	}
 	public void doPost(HttpServletRequest request,HttpServletResponse response) throws ServletException,IOException{
 		doGet(request,response);
